0af130f6f432c101ce51f3bc544e4ca06a159225
Gordon <gburgett@watermark.org>
2018-04-23 16:10:15 -0500

    remove unnecessary inlining plugins

--- webpack.production.js
+++ webpack.production.js
@@ -1,8 +1,6 @@
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const webpack = require('webpack')
-const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin')
-const HtmlWebpackInlineSVGPlugin = require('html-webpack-inline-svg-plugin')
 
 
 const common = require('./webpack.config.js');
@@ -12,8 +10,6 @@ module.exports = merge(common, {
   plugins: [
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
-    }),
-    new HtmlWebpackInlineSourcePlugin(),
-    new HtmlWebpackInlineSVGPlugin()
+    })
   ]
 });
