68b01b5ff6c81590e67c60947ddbce6d46a91f6a
Gordon <gburgett@watermark.org>
2018-05-21 08:58:46 -0500

    Initial HTML for KVPForm

--- src/kvp-editor/kvp-form.spec.tsx
+++ src/kvp-editor/kvp-form.spec.tsx
@@ -5,18 +5,41 @@ import {shallow} from 'preact-render-spy'
 
 import {KVPForm} from './kvp-form'
 
+// tslint:disable:no-unused-expression
+
 describe('<kvp-form />', () => {
   it('should mount', () => {
 
     const rendered = shallow(<KVPForm />)
 
     // assert
-    expect(rendered.text()).to.include('Form')
+    expect(rendered.find('.kvp-form__header')).to.exist
   })
 
-  it('renders two inputs for entering a new kvp')
+  it('renders two inputs for entering a new kvp', () => {
 
-  it('renders a table for displaying the pairs')
+    const rendered = shallow(<KVPForm />)
+
+    // assert
+    const inputs = rendered.find('.kvp-form__form').find('input')
+    expect(inputs.length).to.equal(2)
+    expect(inputs[0].attributes.name).to.equal('key')
+    expect(inputs[1].attributes.name).to.equal('value')
+  })
+
+  it('renders a table for displaying the pairs', () => {
+
+    const rendered = shallow(<KVPForm
+      items={[
+        { key: 'key1', value: 'val1' },
+        { key: 'key2', value: 'val2' },
+      ]}
+      />)
+
+    // assert
+    const rows = rendered.find('.kvp-form__table__row')
+    expect(rows.length).to.equal(2)
+  })
 
   it('adds a new pair to the table on button press')
 
--- src/kvp-editor/kvp-form.tsx
+++ src/kvp-editor/kvp-form.tsx
@@ -1,7 +1,36 @@
 import {Component, h, render} from 'preact'
 
-export class KVPForm extends Component<{}, {}> {
+interface IPair { key: string, value: string }
+
+interface IProps {
+  items?: IPair[]
+}
+
+export class KVPForm extends Component<IProps, {}> {
   public render() {
-    return (<h1>Form</h1>)
+    const {items} = this.props
+
+    return <div>
+        <div className="kvp-form__header">Enter tags as key-value pairs</div>
+        <div className="kvp-form__form">
+          <form>
+            <input type="text" name="key" id="key"></input>
+            <input type="text" name="value" id="value"></input>
+          </form>
+        </div>
+        <div className="kvp-form__table">
+          <table>
+            <tbody>
+              {items && items.map((item) =>
+                  <tr className="kvp-form__table__row">
+                    <td><button>X</button></td>
+                    <td>{item.key}</td>
+                    <td>{item.value}</td>
+                  </tr>,
+                )}
+            </tbody>
+          </table>
+        </div>
+      </div>
   }
 }
